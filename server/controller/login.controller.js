cbfe56822736e472f15ca88e6f09c05f58294f50
yevheniimoskalenko <testjokerqwerty@gmail.com>
2020-05-25 18:35:17 +0300

    return email

--- server/controller/login.controller.js
+++ server/controller/login.controller.js
@@ -4,8 +4,15 @@ require('firebase/auth')
 module.exports = async (req, res) => {
   const { email, password } = req.body
   try {
-    await firebase.auth().signInWithEmailAndPassword(email, password)
-    return res.json({ status: 'success', message: 'Greate you sign in!' })
+    await firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((result) => {
+        return res.json({
+          status: 'success',
+          message: `Greate you sign in!, ${result.user.email}`
+        })
+      })
   } catch (e) {
     return res.json({
       status: 'error',
